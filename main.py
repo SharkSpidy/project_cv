37bab3700822434b9b2597406898b43871e6dcdf
SharkSpidy <josephshibuwork@gmail.com>
2026-03-03 12:23:57 +0530

    Tweak:Numpy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from project-cv!")
-
-
-if __name__ == "__main__":
-    main()
+import numpy
+a = numpy.array([1, 2, 3])
+print(a)
+print(a.shape)
+print(a.dtype)
+print(a.ndim)
+print(a.size)
+print(a.itemsize)
+print(a.nbytes)
